ed9035c08018bb567ab461a452ce1442b64e0b11
rnrnrina <rnrnrina@gmail.com>
2023-04-12 13:08:59 +0800

    Update avggrade.cpp

--- Lab03/avggrade.cpp
+++ Lab03/avggrade.cpp
@@ -1,6 +1,5 @@
 // LAB 3.5 Student Generated Code Assignments
 // Option 1: Write a program that will read in 3 grades from the keyboard and will print the average (to 2 decimal places) of those grades to the screen. It should include good prompts and labeled output. Use the examples from the earlier labs to help you. Notice in the sample run that the answer is stored in fixed point notation with two decimal points of precision.
-// NISRINA FAKHRIATI 1201303193
 
 #include <iostream>
 #include <iomanip>
